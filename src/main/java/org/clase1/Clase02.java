296c1488e10207e7d3b8ffb8fce0f23a85182e45
Manuel Saquilán <manuel.saquilan@gmail.com>
2025-09-05 14:47:14 -0300

    agrego ejercicios 1 y 2

---
+++ src/main/java/org/clase1/Clase02.java
@@ -0,0 +1,29 @@
+package org.clase1;
+
+public class Clase02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad=12;
+		short largo=10000;
+		int	ancho=2000000000;
+		long distancia=700000000000000000L;
+		float peso1=50.25F;
+		double peso2=67.45;
+		boolean existe=true;
+		System.out.println("Contenido de la variable byte:"+edad);
+		System.out.println("Contenido de la variable short:"+largo);
+		System.out.println("Contenido de la variable int:"+ancho);
+		System.out.println("Contenido de la variable long:"+distancia);
+		System.out.println("Contenido de la variable float:"+peso1);
+		System.out.println("Contenido de la variable double:"+peso2);
+		System.out.println("Contenido de la variable boolean:"+existe);
+
+		/*
+		
+		Bloque de comentarios
+		
+		*/
+	}
+
+}
